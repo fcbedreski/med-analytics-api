adf90435e1a58f55e9e42f00ac6942ccb76676ab
fcbedreski <fbedreski@gmail.com>
2026-05-29 12:24:40 -0300

    feat: register upload route inside app root

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import uploadRoutes from './routes/upload.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/upload', uploadRoutes);
 
 export default app;
